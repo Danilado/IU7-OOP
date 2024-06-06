77021e75bf84fd85704d5673509ed48971eef7d2
Danilado <danya.zvyagin@gmail.com>
2024-06-06 17:54:32 +0300

    fix camera

--- lab_03/src/camera/OrthoPTSCAdapter.cpp
+++ lab_03/src/camera/OrthoPTSCAdapter.cpp
@@ -11,7 +11,7 @@ double OrthoPTSCAdapter::shadyExpFunc(double x) {
 std::shared_ptr<Point2D> OrthoPTSCAdapter::convert(const Point3D &pt) {
   Point3D tmppt = transformPoint(pt);
   double fovl = 1600;
-  double persp_coeff = (fovl / shadyExpFunc(fovl + pt.get_z()));
+  double persp_coeff = (fovl / shadyExpFunc(fovl + pt.get_z() - 1600));
   double resx = persp_coeff * pt.get_x();
   double resy = persp_coeff * pt.get_y();
 
